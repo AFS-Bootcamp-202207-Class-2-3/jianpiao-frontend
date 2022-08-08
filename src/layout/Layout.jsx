66f71078bacbd492edef064fa16877f15026a3f6
Cherry <347497274@qq.com>
2022-08-08 16:15:37 +0800

    feat: login

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link, Outlet } from "react-router-dom";
-import Login from '../pages/user/Login';
 
 const Layout = () => {
+  const env = process.env;
   return (
     <div>
       <h1>Layout's Todo</h1>
+      <p>{JSON.stringify(env)}</p>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link>
       </nav>
       <div className="content">
-        <Login />
+        <Outlet />
       </div>
     </div>
   );
