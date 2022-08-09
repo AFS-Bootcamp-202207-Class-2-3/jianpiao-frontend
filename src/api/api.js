06282ca624e0017ef155a440ac2652471b289ec4
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2022-08-09 20:14:34 +0800

    refactor: backend url

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 let baseURL = "http://localhost:8080/"
 const env = JSON.stringify(process.env);
-if(env.indexOf("jianpiao-frontend-staging")!==-1){
-    baseURL="https://jianpiao-banckend-staging.herokuapp.com/"
-}else if(env.indexOf("jianpiao-frontend-production")!==-1){
-    baseURL="https://jianpiao-banckend-production.herokuapp.com/"
-}
+console.log(env)
 
+if(env.indexOf("jianpiao-frontend-staging")!==-1){
+    baseURL="https://jianpiao-backend-staging.herokuapp.com/"
+}else if(env.indexOf("jianpiao-frontend-production")!==-1){
+    baseURL="https://jianpiao-backend-production.herokuapp.com/"
+}
 const api = axios.create({
   baseURL: baseURL,
   withCredentials: true
