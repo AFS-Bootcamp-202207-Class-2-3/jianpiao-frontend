aa34e653291cdb00c55b3ad9535b9046761b7839
TAOSH <taoshaoji@outlook.com>
2022-08-11 20:22:33 +0800

    feat:[#39] Cinema-admin Film Page

--- src/pages/FilmManagement/FilmManagementPage.jsx
+++ src/pages/FilmManagement/FilmManagementPage.jsx
@@ -1,31 +1,46 @@
-import { Button, Form, Input, Modal, Space, Table } from 'antd';
-import { React, useEffect, useState } from 'react';
-import { getAllFilms } from '../../api/film';
-
-const { Column } = Table;
-
+import { Button, Form, Input, message, Modal, Select, Space, Switch, Table } from 'antd';
+import { React, useEffect, useRef, useState } from 'react';
+import { JPApi } from '../../api/http';
+const { Option } = Select;
 
 
-export default function FilmManagementPage() {
+export default function FilmManagementPage(props) {
+    const { TextArea } = Input;
+    const { Column } = Table;
 
-    // const [visible, setVisible] = useState(false);
     const [films, setfilms] = useState([]);
+    const [btnState, setBtnState] = useState("add");
+    const [storeState, setstoreState] = useState([]);
+    const formRef = useRef();
+    const [form] = Form.useForm()
 
     useEffect(() => {
-        const getFilms = async () => {
-            const res = await getAllFilms();
-            setfilms(res.data.films);
-        };
-        getFilms();
+        const getCinemaFilms = async () => {
+            JPApi("admin/films/", "get", { status: "" }, resp => {
+                // console.log(resp.data.data);
+                setfilms(resp.data.data)
+            })
+        }
+        getCinemaFilms();
     }, []);
 
     const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const showModal = () => {
+    const showModal = (record, status) => {
+
         setIsModalVisible(true);
+        if (status === 'update') {
+            setBtnState("update")
+            setstoreState(record);
+            // console.log(storeState);
+            form.setFieldsValue(record);
+        } else {
+            setBtnState("add")
+            form.resetFields();
+        }
     };
 
-    const handleOk = () => {
+    const handleAddFilm = () => {
         setIsModalVisible(false);
     };
 
@@ -33,119 +48,194 @@ export default function FilmManagementPage() {
         setIsModalVisible(false);
     };
 
-    // const handleMenuClick = (e) => {
-    //     if (e.key === '1' || e.key === '2') {
-    //         setVisible(false);
-    //     }
-    // };
 
-    // const handleVisibleChange = (flag) => {
-    //     setVisible(flag);
-    // };
-    // const menu = (
-    //     <Menu
-    //         onClick={handleMenuClick}
-    //         items={[
-    //             {
-    //                 label: '正在上映',
-    //                 key: '1',
-    //             },
-    //             {
-    //                 label: '已下架',
-    //                 key: '2',
-    //             },
-    //         ]}
+    const handleChange = (value) => {
+        JPApi("admin/films/", "get", { status: value }, resp => {
+            setfilms(resp.data.data)
+        })
+        console.log(`selected ${value}`);
+    };
 
-    //     />
-    // );
+
+    const onFinish = (values) => {
+        const { filmName, introduction, posterUrl, duration, releasedTime, director, leadingActor } = values;
+        const film = {
+            filmName,
+            introduction,
+            posterUrl,
+            duration,
+            releasedTime,
+            director,
+            leadingActor,
+        };
+        console.log(film);
+        if (btnState === "add") {
+            JPApi("/admin/films/", "post", film, res => {
+                if (res.data.code === 200) {
+                    message.success('添加电影成功', 5)
+                    setIsModalVisible(false);
+                    formRef.current.resetFields();
+                }
+            })
+        } else {
+            JPApi(`admin/films/${storeState.film_cinema_id}`, "put", film, res => {
+                if (res.data.code === 200) {
+                    message.success('修改电影成功', 5)
+                    setIsModalVisible(false);
+                }
+            })
+        }
+    }
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    function copy(obj) {
+        var newobj = obj.constructor === Array ? [] : {};
+        if (typeof obj !== 'object') {
+            return;
+        }
+        for (var i in obj) {
+            newobj[i] = typeof obj[i] === 'object' ? copy(obj[i]) : obj[i];
+        }
+        return newobj
+    }
+
+    const onChange = (checked, record) => {
+        console.log(record.film_cinema_id)
+        if (checked) {
+            const film = { filmCinemaId: record.film_cinema_id, status: "showing" };
+            JPApi("admin/films/update-film-cinema-status", "post", film, res => {
+                if (res.data.code === 200) {
+                    record.status = "showing";
+                    message.success('电影成功上架', 5)
+                    setIsModalVisible(false);
+                }
+            })
+        } else {
+            const film = { filmCinemaId: record.film_cinema_id, status: "seal" };
+            JPApi("admin/films/update-film-cinema-status", "post", film, res => {
+                if (res.data.code === 200) {
+                    record.status = "seal"
+                    message.success('电影成功下架', 5)
+                    setIsModalVisible(false);
+                }
+            })
+        }
+   
+        window.location.reload();
+    }
+
 
     return (
         <div>
-            <Button type="primary" onClick={showModal}>
-                添加电影
-            </Button>
-            <Modal title="添加电影" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            <Space>
+                <Button type="primary" onClick={showModal}>
+                    添加电影
+                </Button>
+
+                <Select
+                    defaultValue="全部影片"
+                    style={{
+                        width: 120,
+                    }}
+                    onChange={handleChange}
+                >
+                    <Option value="">全部影片</Option>
+                    <Option value="seal">已下架</Option>
+                    <Option value="showing">正在上映</Option>
+                </Select>
+            </Space>
+            <Modal title={btnState === "add" ? "添加电影" : "修改电影"} visible={isModalVisible} onOk={handleAddFilm} onCancel={handleCancel}
+                footer={
+                    []
+                }
+            >
                 <Form
-                    name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
+                    name="film-add"
+                    ref={formRef}
+                    form={form}
                     initialValues={{ remember: true }}
                     autoComplete="off"
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
                 >
                     <Form.Item
                         label="电影名称"
                         name="filmName"
-                    >
-                        <Input />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="电影简介"
-                        name="introduction"
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        label="上映日期"
-                        name="releasedTime"
-                        // rules={[{ required: true, message: 'Please input your releasedTime!' }]}
+                        rules={[{ required: true, message: 'Please input your filmName!' }]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="时长"
                         name="duration"
-                        // rules={[{ required: true, message: 'Please input your duration!' }]}
+                        rules={[{ required: true, message: 'Please input your duration!' }]}
                     >
-                        <Input />
+                        <Input placeholder="分钟" />
                     </Form.Item>
                     <Form.Item
                         label="导演"
                         name="director"
-                        // rules={[{ required: true, message: 'Please input your director!' }]}
+                        rules={[{ required: true, message: 'Please input your director!' }]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
                         label="主演"
                         name="leadingActor"
-                        // rules={[{ required: true, message: 'Please input your leadingActor!' }]}
+                        rules={[{ required: true, message: 'Please input your leadingActor!' }]}
                     >
                         <Input />
                     </Form.Item>
                     <Form.Item
-                        label="评分"
-                        name="score"
-                        // rules={[{ required: true, message: 'Please input your score!' }]}
+                        label="上映日期"
+                        name="releasedTime"
+                        rules={[{ required: true, message: 'Please input your releasedTime!' }]}
                     >
-                        <Input />
+                        <Input placeholder="yyyy-MM-dd" />
                     </Form.Item>
+                    <Form.Item
+                        label="电影海报"
+                        name="posterUrl"
+                        rules={[{ required: true, message: 'Please input your posterUrl!' }]}
+                    >
+                        <Input placeholder="海报链接" />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="电影简介"
+                        name="introduction"
+                        rules={[{ required: true, message: 'Please input your introduction!' }]}
+                    >
+                        <TextArea rows={3} placeholder="电影简介" />
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit" style={{ float: "right" }} >
+                        {btnState === "add" ? "添加" : "修改"}
+                    </Button>
                 </Form>
             </Modal>
-            {/* <Dropdown overlay={menu} onVisibleChange={handleVisibleChange} visible={visible}>
-                <a onClick={(e) => e.preventDefault()}>
-                    <Space>
-                        Hover me
-                        <DownOutlined />
-                    </Space>
-                </a>
-            </Dropdown> */}
-            <Table dataSource={films}>
+
+            <Table dataSource={films} rowKey={record => record.film_cinema_id + record.id}>
+                <Column title="影院号" dataIndex="cinemaId" key="cinemaId" />
                 <Column title="电影名称" dataIndex="filmName" key="filmName" />
-                <Column title="电影简介" dataIndex="introduction" key="introduction" />
                 <Column title="上映日期" dataIndex="releasedTime" key="releasedTime" />
-                <Column title="时长" dataIndex="duration" key="address" />
+                <Column title="时长" dataIndex="duration" key="duration" />
                 <Column title="导演" dataIndex="director" key="director" />
                 <Column title="演员" dataIndex="leadingActor" key="leadingActor" />
-                <Column title="评分" dataIndex="score" key="score" />
+                <Column title="电影简介" dataIndex="introduction" key="introduction" />
+                {/* <Column title="电影海报" dataIndex="posterUrl" key="posterUrl" /> */}
+                <Column title="状态" dataIndex="status" key="status" render={(text) => { return text === "showing" ? <span>上映中</span> : <span>已下架</span> }} />
 
                 <Column
                     title="操作"
                     key="action"
                     render={(_, record) => (
-                        <Space size="middle">
-                            {/* <a>Invite {record.lastName}</a>
-                            <a>Delete</a> */}
+                        <Space size="large">
+                            <Button type="primary" onClick={() => showModal(record, "update")}>修改</Button>
+                            {/* <Button type="primary" onClick={() => onChange(record.status === "showing", record)}>{record.status === "showing" ? '下架' : '上架'}</Button> */}
+                            <Switch checkedChildren="上架" unCheckedChildren="下架" defaultChecked={record.status === "showing"} onChange={(checked) => onChange(checked, record)} />
                         </Space>
                     )}
                 />
@@ -153,5 +243,4 @@ export default function FilmManagementPage() {
         </div>
     )
 }
-
 
