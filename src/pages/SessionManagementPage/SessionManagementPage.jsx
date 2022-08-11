1cd0b4568de44dbe4bbed054a5b7ca8db30a0dc7
renry3 <ryan.ren@oocl.com>
2022-08-11 18:06:06 +0800

    feat:[#44] add session modal

--- src/pages/SessionManagementPage/SessionManagementPage.jsx
+++ src/pages/SessionManagementPage/SessionManagementPage.jsx
@@ -1,11 +1,24 @@
-import { Button, message, Table } from "antd";
+import {
+  Button,
+  DatePicker,
+  Form,
+  InputNumber,
+  message,
+  Modal,
+  Select,
+  Space,
+  Table,
+  TimePicker,
+} from "antd";
 import React, { useEffect, useState } from "react";
 import { JPApi } from "../../api/http";
 
 const SessionManagementPage = () => {
   const userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
   const [data, setData] = useState([]);
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { Option } = Select;
+  const [form] = Form.useForm();
   useEffect(() => {
     JPApi(`/user/${userInfo.id}/sessions`, "get", {}, (response) => {
       setData([...response.data.data]);
@@ -13,11 +26,29 @@ const SessionManagementPage = () => {
     // eslint-disable-next-line
   }, []);
 
-  const deleteItem = (sessionId) => {
+  const deleteSession = (sessionId) => {
     JPApi(`sessions/${sessionId}`, "delete", {}, () => {
       message.success("删除成功！");
-      setData([...data.filter(session => session.id !== sessionId)]);
+      setData([...data.filter((session) => session.id !== sessionId)]);
     });
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    form.submit();
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleSubmit = (session) => {
+    debugger;
+    console.log(session);
   };
 
   const columns = [
@@ -50,7 +81,7 @@ const SessionManagementPage = () => {
       dataIndex: "",
       key: "delete",
       render: (record) => (
-        <Button type="primary" danger onClick={() => deleteItem(record.id)}>
+        <Button type="primary" danger onClick={() => deleteSession(record.id)}>
           删除
         </Button>
       ),
@@ -59,6 +90,69 @@ const SessionManagementPage = () => {
 
   return (
     <div>
+      <Space style={{ marginBottom: 16 }}>
+        <Button type="primary" onClick={showModal}>
+          新增
+        </Button>
+        <Modal
+          title="新增场次"
+          visible={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <Form
+            labelCol={{
+              span: 6,
+            }}
+            wrapperCol={{
+              span: 15,
+            }}
+            onFinish={handleSubmit}
+            form={form}
+          >
+            <Form.Item name="film" label="电影">
+              <Select>
+                <Option value="demo">1</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name="cinema" label="影厅">
+              <Select>
+                <Option value="demo">1</Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name="date" label="放映日期">
+              <DatePicker
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Item>
+            <Form.Item name="startTime" label="开始时间">
+              <TimePicker
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Item>
+            <Form.Item name="endTime" label="结束时间">
+              <TimePicker
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Item>
+            <Form.Item name="price" label="价格">
+              <InputNumber
+                prefix="￥"
+                min={0}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </Space>
       <Table columns={columns} dataSource={data} />
     </div>
   );
