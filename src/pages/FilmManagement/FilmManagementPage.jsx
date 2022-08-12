a8a79b2de98c5fe6deb25fd0eab54307e077ad70
TAOSH <taoshaoji@outlook.com>
2022-08-12 10:03:48 +0800

    fix:[#39] Cinema-admin Film Page after add film no status

--- src/pages/FilmManagement/FilmManagementPage.jsx
+++ src/pages/FilmManagement/FilmManagementPage.jsx
@@ -78,6 +78,7 @@ export default function FilmManagementPage(props) {
                     film.status = "showing";
                     setfilms([film, ...films]);
                     formRef.current.resetFields();
+                    getCinemaFilms();
                 }
             })
         } else {
